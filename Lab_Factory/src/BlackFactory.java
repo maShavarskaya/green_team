0be63ef566d2d4388ca0d69d5929c42ec9b61813
FedorenkoAnna <feanneta@gmail.com>
2020-10-14 15:21:43 +0300

    создан BlackFactory

--- Lab_Factory/src/BlackFactory.java
+++ Lab_Factory/src/BlackFactory.java
@@ -1,1 +1,25 @@
+package com.company;
 
+public class BlackFactory extends BaseFactory {
+
+    private final String message ="This is Black Factory";
+
+    @Override
+    public Circle createCircle(double x, double y, double radius) {
+        return new BlackCircle(x,y,radius);
+    }
+
+    @Override
+    public Triangle createTriangle(double a, double b, double c) {
+        return new BlackTriangle(a,b,c);
+    }
+
+    public BlackFactory() {
+        System.out.println(message);
+    }
+
+    @Override
+    public BaseFactory getFactory() {
+        return this;
+    }
+}
