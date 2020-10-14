b94a4f9f78fd2c0780036aa297f4b6f4d0277ce6
liutavaleria <49949962+liutavaleria@users.noreply.github.com>
2020-10-14 12:38:28 +0300

    White Circle update

--- Lab_Factory/src/WhiteCircle.java
+++ Lab_Factory/src/WhiteCircle.java
@@ -1,1 +1,38 @@
+package com.company;
 
+public class WhiteCircle extends WhiteFactory implements Circle {
+    double x ;
+    double y ;
+    double radius;
+
+    {
+        x =0;
+        y = 0;
+        radius = 1;
+    }
+
+    public WhiteCircle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public WhiteCircle() {
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Created white circle with center in "+
+                "("+ x +
+                " ; " + y + ")"+
+                " and radius "+ radius;
+    }
+
+    @Override
+    public double countArea() {
+        return  Math.PI* Math.pow(radius,2);
+    }
+
+}
