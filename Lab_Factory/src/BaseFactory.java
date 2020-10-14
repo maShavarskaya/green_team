f9e7b020e19f19d64342ccea288089e80d8d5f93
OleksandrPiven <63542152+OleksandrPiven@users.noreply.github.com>
2020-10-14 18:58:19 +0300

    Update BaseFactory.java
    changed class BaseFactory

--- Lab_Factory/src/BaseFactory.java
+++ Lab_Factory/src/BaseFactory.java
@@ -1,2 +1,5 @@
-// creareCircle() : void
-// creareTriangle() : void
+public abstract class BaseFactory {
+    abstract Circle createCircle(int radius);
+
+    abstract Triangle createTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
+}
