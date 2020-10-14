7a3ad55710b6000bf1c2b40d4595e995ccdc92d6
maShavarskaya <58817912+maShavarskaya@users.noreply.github.com>
2020-10-14 13:47:52 +0300

    Update BlackCircle.java

--- Lab_Factory/src/BlackCircle.java
+++ Lab_Factory/src/BlackCircle.java
@@ -1,1 +1,32 @@
+package com.company;
 
+public class BlackCircle extends BlackFactory implements Circle {
+    double x ;
+    double y ;
+    double radius;
+
+    {
+        x =0;
+        y = 0;
+        radius = 1;
+    }
+
+    public BlackCircle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Created black circle with center in "+
+                "("+ x +
+                " ; " + y + ")"+
+                " and radius "+ radius;
+    }
+
+    @Override
+    public double countArea() {
+        return Math.PI* Math.pow(radius,2);
+    }
+}
