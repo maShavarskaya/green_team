2bd5a2212425a3e20f5418f65f95a39d0d78de93
FedorenkoAnna <feanneta@gmail.com>
2020-10-16 14:58:03 +0300

    delete method

--- Lab_Factory/src/BlackFactory.java
+++ Lab_Factory/src/BlackFactory.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class BlackFactory extends BaseFactory { // WhiteFactory from BaseFactory
 
-    private final String message ="This is Black Factory";
+    private final String message = "This is Black Factory";
 
     @Override
     //create a black circle(radius)
@@ -11,7 +11,7 @@ public class BlackFactory extends BaseFactory { // WhiteFactory from BaseFactory
     }
 
     @Override
-    //create a white triangle (x1,y1); (x2,y2); (x3,y3) 
+    //create a white triangle (x1,y1); (x2,y2); (x3,y3)
     public Triangle createTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
         return new BlackTriangle(x1, y1, x2, y2, x3, y3);
     }
@@ -21,8 +21,4 @@ public class BlackFactory extends BaseFactory { // WhiteFactory from BaseFactory
         return "BlackFactory";
     }
 
-    @Override
-    public BaseFactory getFactory() {
-        return this;
-    }
 }
